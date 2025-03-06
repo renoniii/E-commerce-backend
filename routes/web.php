be653595e2122b648a6ec98268306a0e5b4906ff
Vanessa Rincón <krincon486@unab.edu.co>
2025-03-06 00:19:33 -0500

    Rutas básicas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    echo "LISTADO DE TODOS LOS PRODUCTOS";
+});
+
+Route::get('/products/create', function () {
+    echo "FORMULARIO PARA CREAR UN PRODUCTO";
+});
+
+Route::get('/products/{name}', function ($name) {
+    echo "PRODUCTO: $name";
+});
+
+Route::get('/products/{name}/{category}', function ($name, $category) {
+    echo "PRODUCTO: $name, y tiene una categoria $category";
+});
